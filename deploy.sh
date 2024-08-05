ac334537984868e2880df0105f086f8fa6f98831
olegbaranovskyi <153509142+olegbaranovskyi@users.noreply.github.com>
2024-08-05 17:23:05 +0300

    Auto git push ...

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
-ng build --output-path docs --base-href /cards/
-git add .
-git commit -a -m "Auto message"
-git push
+# ng build --output-path docs --base-href /cards/
+# git add .
+# git commit -a -m "Auto message"
+# git push
+
+ng deploy
